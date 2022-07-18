d7aba20d3ee4020d7e622e00499c1be4fca0d551
zlogzr <zylogzr@163.com>
2022-07-18 18:13:08 +0800

    style: 优化项目列表页面

--- src/authenticated-app/index.tsx
+++ src/authenticated-app/index.tsx
@@ -1,12 +1,53 @@
+import { ReactComponent as SoftwareLogo } from '@/assets/software-logo.svg'
+import { Row } from '@/components/lib'
 import { useAuth } from '@/context/auth-context'
 import { ProjectList } from '@/pages/project-list'
+import styled from '@emotion/styled'
+import { Dropdown, Menu } from 'antd'
 
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth()
+  const { logout, user } = useAuth()
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectList />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key={'logout'}>
+                  <a onClick={logout}>登出</a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a onClick={e => e.preventDefault()}>Hi, {user?.name}</a>
+          </Dropdown>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectList />
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`
+
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`
+const HeaderLeft = styled(Row)``
+const HeaderRight = styled.div``
+const Main = styled.main``
--- src/pages/project-list/index.tsx
+++ src/pages/project-list/index.tsx
@@ -1,8 +1,10 @@
-import { List } from './list'
-import { SearchPanel } from './search-panel'
 import { cleanObject, useDebounce, useMount } from '@/utils'
 import { useHttp } from '@/utils/http'
+import styled from '@emotion/styled'
 import { useEffect, useState } from 'react'
+
+import { List } from './list'
+import { SearchPanel } from './search-panel'
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -27,9 +29,14 @@ export const ProjectList = () => {
   })
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  padding: 3.2rem;
+`
